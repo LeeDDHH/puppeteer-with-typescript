4206480ffba4c1f6a6550ddf1da5ed79c4320a45
DongHeon Lee <niche3600@gmail.com>
2022-04-16 20:01:30 +0900

    add use class case

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,13 @@ import puppeteer from 'puppeteer';
 const screenShotFromURL = async (url: string) => {
   // Google Chromeを起動して処理をする場合
   // const browser = await puppeteer.launch(Const.LAUNCH_GOOGLE_CHROME_PROPS);
+  // const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  // await page.setViewport({ width: 1200, height: 800 });
   await page.goto(url);
+  await page.waitForSelector('.gLFyf');
+  await page.type('.gLFyf', 'Puppeteer');
   await page.screenshot({ path: 'example.png' });
   await browser.close();
 };
